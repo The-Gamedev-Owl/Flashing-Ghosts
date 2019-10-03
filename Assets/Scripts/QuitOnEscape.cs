1348b3365ae53199b0a120afa82cf117ff748b6f
Maxime Lambotin <maxime.lambotin@epitech.eu>
2019-10-03 10:42:11 +0900

    feat: add main menu

---
+++ Assets/Scripts/QuitOnEscape.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class QuitOnEscape : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey ("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/EventHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class EventHandler : MonoBehaviour
+    {
+        public void OnPlayButtonClicked()
+        {
+            // TODO: Update with the actual scene level
+            // SceneManager.LoadScene("Level");
+        }
+
+        public void OnQuitButtonClicked()
+        {
+            Application.Quit();
+        }
+    }
+}
