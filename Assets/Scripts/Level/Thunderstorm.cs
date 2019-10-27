9fb77dde160040e1a5214cf7b0ab5c6448296900
Maxime Lambotin <maxime.lambotin@epitech.eu>
2019-10-27 14:44:49 +0900

    feat: add thunder storm random event with sound and visuals

---
+++ Assets/Scripts/Level/Thunderstorm.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thunderstorm : MonoBehaviour
+{
+    private GameObject[] lights;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        lights = GameObject.FindGameObjectsWithTag("ThunderLight");
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(StrikeThunder());
+    }
+
+    private IEnumerator StrikeThunder()
+    {
+        yield return new WaitForSeconds(Mathf.Lerp(5, 10, Random.value));
+        while (true)
+        {
+            foreach (var light in lights)
+            {
+                var animator = light.GetComponent<Animator>();
+                if (animator)
+                {
+                    animator.SetTrigger("StrikeThunder");
+                }
+            }
+
+            audioSource.Play();
+            yield return new WaitForSeconds(Mathf.Lerp(10, 20, Random.value));
+        }
+    }
+}
